481c39c5527099b1467da996d749392e38019995
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2024-03-07 12:54:43 +0530

    files updated

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FormulaPackage",
-            resources: [.copy("Formula/20230022.json")]),
+            resources: [.copy("Resources/20230022.json")]),
         .testTarget(
             name: "FormulaPackageTests",
             dependencies: ["FormulaPackage"]),
